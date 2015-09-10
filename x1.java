a53189796217cef9700b2f6e6cb58b1b45b126a3
Puled81 <drakep@verizon.net>
2015-09-09 20:38:14 -0400

    Puppet partial house

--- x1.java
+++ x1.java
@@ -1,14 +1,14 @@
-//////// What am I trying to do???
-//////// What is my name?  (CST 112; today's date?)
+//Making a Scene
+//Drake P  9/9/15
 
-//// GLOBALS:  coordinates, speed, etc.
+/// GLOBALS:  coordinates, speed, etc.
 float x, y;       // Position of creature.
 float dx, dy;     // Speed.
 float horizon;
 
 //// SETUP:  window size, initialization (start in middle of screen).
 void setup() {
-  size( 640,480);
+  size( 700,700);
   horizon=  height/4;
   x=  width/2;
   y=  height/2;
@@ -19,7 +19,7 @@ void setup() {
 //// NEXT FRAME:  scene, action, show.
 void draw() {
   //// SCENE:  sky, sun, tree, house, etc.
-  background( 100,150,200 );                // sky
+  background( 100,200,250 );                // sky
   fill( 255,255,0 );
   ellipse( width*3/4, height/8, 40,40 );    // sun
   // Grass
@@ -27,12 +27,14 @@ void draw() {
   rect( 0,horizon, width,height*3/4 );      // grass.
   
   /* INSERT YOUR CODE HERE! */
-  triangle( 150,horizon, 120,horizon-50, 180,horizon-50  );  // tree
-  text( "This is NOT a good tree; please fix it!", 150,horizon );
-                                            // house
+  fill(230);
+  rect( 150,horizon, 120,horizon-50);  // house
+  triangle(50,20,20,20,20,20);
+  
+                                      
 
   fill(0);
-  text( "My name is Mud", 10,height-20 );                                          
+  text( "My name is Drake", 10,height-20 );                                          
                                             
   //// ACTION:  move (x,y) coordinates.
   x=  x + dx;
@@ -41,8 +43,13 @@ void draw() {
   //// SHOW:  display the creature at (x,y)
 
   /* INSERT YOUR CODE HERE! */
-  fill(255,0,0); rect( x,y, 30,50 );        /* REPLACE THIS WITH YOUR OWN CODE! */
-  text( "Fred", x,y );
+  fill(50,80,150); 
+  rect( x,y, 30,50 );
+  fill(255,0,0);
+  ellipse(x+15,y-11,20,20);  /* REPLACE THIS WITH YOUR OWN CODE! */
+  triangle(x+15,y-11,20,20,20,20);
+  fill(255);
+  text( "Jimmy", x,y+30 );
 
 }
 
@@ -62,5 +69,3 @@ void keyPressed() {
   }
 }
    
-   
-
